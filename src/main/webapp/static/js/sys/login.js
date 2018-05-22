6c274da47e7586b7bc00003e8fcb460bbbbc23f9
couzuwugezi <493749283@qq.com>
2018-05-22 22:27:36 +0800

    shiro登陆完成

--- src/main/webapp/static/js/sys/login.js
+++ src/main/webapp/static/js/sys/login.js
@@ -59,26 +59,42 @@ $(document).ready(function () {
     });
 
     let dialog = new Vue({
-            el: "#dialog",
-            data: {
-                show: false,
-                form: {},
-                sexOptions: [{
-                    value: '1',
-                    label: '男'
-                }, {
-                    value: '0',
-                    label: '女'
-                }],
-                rules: {
-                    loginName: [
+        el: "#dialog",
+        data: {
+            show: false,
+            form: {},
+            sexOptions: [{
+                value: '1',
+                label: '男'
+            }, {
+                value: '0',
+                label: '女'
+            }],
+            rules: {
+                loginName: [
+                    {required: true, message: '该项不能为空', trigger: 'blur'},
+                    {min: 1, max: 20, message: '长度在 1 到 50 个字符', trigger: 'blur'},
+                    {
+                        validator: function (rule, value, callback) {
+                            let zzjgdmRegex = /^(\w|[0-9])*$/;
+                            if (!zzjgdmRegex.test(value)) {
+                                callback(new Error('只能输入英文和数字！'))
+                            } else {
+                                callback();
+                            }
+                        },
+                        trigger: 'blur'
+                    }
+                ],
+                realName:
+                    [
                         {required: true, message: '该项不能为空', trigger: 'blur'},
                         {min: 1, max: 20, message: '长度在 1 到 50 个字符', trigger: 'blur'},
                         {
                             validator: function (rule, value, callback) {
-                                let zzjgdmRegex = /^(\w|[0-9])*$/;
-                                if (!zzjgdmRegex.test(value)) {
-                                    callback(new Error('只能输入英文和数字！'))
+                                let nameRegex = /^[\u4E00-\u9FFFa-zA-Z]+$/;
+                                if (!nameRegex.test(value)) {
+                                    callback(new Error('只能输入中文英文！'))
                                 } else {
                                     callback();
                                 }
@@ -86,78 +102,61 @@ $(document).ready(function () {
                             trigger: 'blur'
                         }
                     ],
-                    realName:
-                        [
-                            {required: true, message: '该项不能为空', trigger: 'blur'},
-                            {min: 1, max: 20, message: '长度在 1 到 50 个字符', trigger: 'blur'},
-                            {
-                                validator: function (rule, value, callback) {
-                                    let nameRegex = /^[\u4E00-\u9FFFa-zA-Z]+$/;
-                                    if (!nameRegex.test(value)) {
-                                        callback(new Error('只能输入中文英文！'))
-                                    } else {
-                                        callback();
-                                    }
-                                },
-                                trigger: 'blur'
-                            }
-                        ],
-                    phone:
-                        [
-                            {required: true, message: '该项不能为空', trigger: 'blur'},
-                            {min: 1, max: 11, message: '长度在 1 到 11 个字符', trigger: 'blur'},
-                            {
-                                validator: function (rule, value, callback) {
-                                    let telRegex = /^([0-9]|[\-])*$/;
-                                    if (!telRegex.test(value)) {
-                                        callback(new Error('只能输入数字和-！'))
-                                    } else {
-                                        callback();
-                                    }
-                                },
-                                trigger: 'blur'
-                            }
-                        ],
-                    sex:
-                        [
-                            {required: true, message: '该项不能为空', trigger: 'blur'}
-                        ],
-                    loginPassword:
-                        [
-                            {required: true, message: '该项不能为空', trigger: 'blur'},
-                            {min: 1, max: 20, message: '长度在 1 到 50 个字符', trigger: 'blur'}
-                        ],
-                }
-            },
-            methods: {
-                submitForm: function (formName) {
-                    this.$refs[formName].validate((valid) => {
-                        if (valid) {
-                            $.ajax({
-                                url: Lee.realPath + "/register",
-                                type: "POST",
-                                dataType: "json",
-                                data: JSON.stringify(dialog.form),
-                                async: false,
-                                contentType: "application/json",
-                                success: function (response) {
-                                    debugger;
-                                    dialog.form = {};
-                                    dialog.show = false;
-                                    dialog.$alert(response.msg);
-                                },
-                                error: function (error) {
-                                    dialog.$alert("系统异常!");
-                                    console.log(error);
+                phone:
+                    [
+                        {required: true, message: '该项不能为空', trigger: 'blur'},
+                        {min: 1, max: 11, message: '长度在 1 到 11 个字符', trigger: 'blur'},
+                        {
+                            validator: function (rule, value, callback) {
+                                let telRegex = /^([0-9]|[\-])*$/;
+                                if (!telRegex.test(value)) {
+                                    callback(new Error('只能输入数字和-！'))
+                                } else {
+                                    callback();
                                 }
-                            })
-                        } else {
-                            console.log('error submit!!');
-                            return false;
+                            },
+                            trigger: 'blur'
                         }
-                    });
-                }
+                    ],
+                sex:
+                    [
+                        {required: true, message: '该项不能为空', trigger: 'blur'}
+                    ],
+                loginPassword:
+                    [
+                        {required: true, message: '该项不能为空', trigger: 'blur'},
+                        {min: 1, max: 20, message: '长度在 1 到 50 个字符', trigger: 'blur'}
+                    ],
             }
-        })
-    ;
+        },
+        methods: {
+            submitForm: function (formName) {
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        $.ajax({
+                            url: Lee.realPath + "/register",
+                            type: "POST",
+                            dataType: "json",
+                            data: JSON.stringify(dialog.form),
+                            async: false,
+                            contentType: "application/json",
+                            success: function (response) {
+                                debugger;
+                                dialog.form = {};
+                                dialog.show = false;
+                                dialog.$alert(response.msg);
+                            },
+                            error: function (error) {
+                                dialog.$alert("系统异常!");
+                                console.log(error);
+                            }
+                        })
+                    } else {
+                        console.log('error submit!!');
+                        return false;
+                    }
+                });
+            }
+        }
+    });
 });
