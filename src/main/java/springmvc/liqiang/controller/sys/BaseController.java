941339498430e922d9184b41a40299e33ee0457b
couzuwugezi <493749283@qq.com>
2018-05-23 22:54:23 +0800

    shiro权限

--- src/main/java/springmvc/liqiang/controller/sys/BaseController.java
+++ src/main/java/springmvc/liqiang/controller/sys/BaseController.java
@@ -5,7 +5,7 @@ import springmvc.liqiang.utils.ReturnFormat;
 /**
  * @author liqiang
  * 时间 2018-05-23 22:43
- * 描述
+ * 描述 exception handler
  */
 public abstract class BaseController {
     protected String retContent(int status, Object data) {
